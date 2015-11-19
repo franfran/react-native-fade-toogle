c36fcde354399f294248e0d7d07c14cc2bbfbf37
franfran <francis@franfran.com>
2015-11-19 14:37:50 +0800

    0.1.0 source code commit

---
+++ FadeToogle.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+    PropTypes,
+    StyleSheet,
+    Components,
+    Animated,
+    Text,
+    View,
+    Image,
+} = React;
+
+var FadeToogle = React.createClass({
+    propTypes: {
+        fromOpacity: PropTypes.number,
+        toOpacity: PropTypes.number,
+        duration: PropTypes.number,
+        component: PropTypes.string,
+        easing: PropTypes.func,
+        callback: PropTypes.func,
+        style: View.propTypes.style,
+    },
+    getDefaultProps() {
+        return {
+            fromOpacity: 1,
+            toOpacity: 0,
+            duration: 100,
+        };
+    },
+    getInitialState() {
+        return {
+            opacity: this.props.fromOpacity,
+            fadeAnim: new Animated.Value(this.props.fromOpacity),
+        };
+    },
+    fadeToggle() {
+        var that = this;
+        var callback = function(){
+            that.props.callback();
+        };
+
+        var t;
+        if(this.state.opacity == this.props.toOpacity){
+            t = this.props.fromOpacity;
+        }else{
+            t = this.props.toOpacity;
+        }
+        this.setState({opacity: t});
+        Animated.timing(
+            this.state.fadeAnim,
+            {
+                toValue: t,
+                duration: this.props.duration,
+                easing: this.props.easing
+            },
+        ).start(callback);
+    },
+    render(){
+        var com = eval('Animated.'+this.props.component);
+        var child = eval(this.props.children);
+        return React.createElement(com, {style: [this.props.style, {opacity: this.state.fadeAnim}]}, child);
+    }
+});
+
+module.exports = FadeToogle;
